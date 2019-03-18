dc6be2b04d51433660be6a642820304eb3f92250
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-03-18 15:56:56 -0500

    Completed color enum:

--- GamePiece.java
+++ GamePiece.java
@@ -1,4 +1,5 @@
 
-public enum GamePiece {
-
+public enum GamePiece 
+{
+	
 }
---
+++ Todo.java
@@ -0,0 +1,56 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-03-18
+OU ID_______________: 113410896
+OU EMAIL____________: jonathan.g.miller-1@ou.edu
+Tasks:
+1. Complete Color Enum
+2. Complete Shape Enum
+3. Complete Location Enum
+4. Complete GamePieceAppearance class
+5. Complete GamePiece enum
+6. Complete BoardGame class
+7. Debug
+8. Write Color tests
+9. Write Shape tests
+10. Write Location tests
+11. Write GamePieceAppearance tests
+12. Write GamePiece tests
+13. Write BoardGame tests
+14. Debug tests
+15. Comment
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 10
+6. 15
+7. 15
+8. 10
+9. 10
+10. 10
+11. 10
+12. 10
+13. 15
+14. 20
+15. 10
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
+12. 
+13. 
+14. 
+15. 
+*/}
