1c31db264bd8387cc5ba93a6df1c1c7d5776fd78
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-03-18 16:10:45 -0500

    Finished gamePieceAppearance

--- GamePieceAppearance.java
+++ GamePieceAppearance.java
@@ -1,4 +1,24 @@
 
-public class GamePieceAppearance {
+public class GamePieceAppearance 
+{
+	//Variables to hold the appearance of the game piece
+	private Shape s;
+	private Color c;
+	
+	//Constructor to fill the above variables
+	public GamePieceAppearance(Color ic, Shape is)
+	{
+		s = is;
+		c = ic;
+	}
+	
+	public Shape getShape() 
+	{
+		return s;
+	}
 
+	public Color getColor() 
+	{
+		return c;
+	}
 }
